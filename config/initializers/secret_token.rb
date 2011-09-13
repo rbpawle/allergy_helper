9aac2ce4f74e3d874bc66a449debe712fcd13dd4
Reggie Pawle <rbpawle@gmail.com>
2011-09-13 07:47:08 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AllergyHelper::Application.config.secret_token = '3f40fad8a8a94d711e48904fe9da4d01c839e9f6b4f70ec9846601963021516d0c35172f89aca73b7567c232ae36a8d890a296a49b78c7652efaf1039d1de22a'
